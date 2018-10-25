cbb5a5abc8e4cb08ab462bc92d18d7758ba35b62
Benedict Idonije <bidonije@googlemail.com>
2018-10-25 21:16:29 +0100

    add mvc to project

--- src/main/java/guru/springframework/spring5webapp/model/Publisher.java
+++ src/main/java/guru/springframework/spring5webapp/model/Publisher.java
@@ -11,10 +11,15 @@ public class Publisher {
     private String name;
     private String address;
 
+    public Publisher() {
+    }
+
     public Publisher(String name, String address) {
         this.name = name;
         this.address = address;
     }
+
+
 
     public String getName() {
         return name;
